b2f7f0fcb78d97d2806d07ad94592312f83cf6cb
rosiananatalie <rosiananatalie15@gmail.com>
2023-06-23 20:27:19 -0400

    try to play the mp3

--- src/component/VideoPlayerComponent.js
+++ src/component/VideoPlayerComponent.js
@@ -1,39 +1,54 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Player, BigPlayButton, ControlBar, ProgressControl, CurrentTimeDisplay, TimeDivider, DurationDisplay } from 'video-react';
 import PlayToggle from 'video-react/lib/components/control-bar/PlayToggle';
-// import videos from '../../assets/ed1.mp4'
-
 
 function VideoPlayerComponent(props) {
-    
-    const videoId = props.videoId
-    const videoType = props.videoType
-    const videoRef = props.videoRef
-    return (
-        <div>
-            <Player
-                ref={props.videoRef}
-                playsInline
-                poster = {"../../assets/trial1.png"}
-                controls
-            >
-                    {/* <source src={require("../assets/" + videoId + ".mp4")} type="video/mp4"/> */}
-                    <source src={"assets/" + videoType + videoId + ".mp4"} type="video/mp4"/>
-                    <BigPlayButton position="center" />
+  const { videoId, videoType, videoRef, mp3Files } = props;
 
+  useEffect(() => {
+    const videoElement = videoRef.current.video.video;
+    const audioElements = mp3Files.map((mp3File) => new Audio(mp3File.src));
 
-                    <ControlBar autoHide={false} disableDefaultControls>
-                        <PlayToggle/>
-                        <CurrentTimeDisplay />
-                        <TimeDivider />
-                        <DurationDisplay />
-                        <ProgressControl/>
-                    </ControlBar>
-            </Player>
+    videoElement.addEventListener('timeupdate', handleTimeUpdate);
 
-        </div>
-          
-    );
+    function handleTimeUpdate() {
+      const currentTime = videoElement.currentTime;
+
+      // Play the corresponding audio file if its start time matches the video time
+      audioElements.forEach((audio) => {
+        if (currentTime >= audio.startTime && currentTime < audio.endTime && audio.paused) {
+          audio.play();
+        } else if ((currentTime < audio.startTime || currentTime >= audio.endTime) && !audio.paused) {
+          audio.pause();
+        }
+      });
+    }
+
+    return () => {
+      videoElement.removeEventListener('timeupdate', handleTimeUpdate);
+      audioElements.forEach((audio) => {
+        audio.pause();
+        audio.currentTime = 0;
+      });
+    };
+  }, [videoRef, mp3Files]);
+
+  return (
+    <div>
+      <Player ref={videoRef} playsInline poster="../../assets/trial1.png" controls>
+        <source src={`assets/${videoType}${videoId}.mp4`} type="video/mp4" />
+        <BigPlayButton position="center" />
+
+        <ControlBar autoHide={false} disableDefaultControls>
+          <PlayToggle />
+          <CurrentTimeDisplay />
+          <TimeDivider />
+          <DurationDisplay />
+          <ProgressControl />
+        </ControlBar>
+      </Player>
+    </div>
+  );
 }
 
-export default VideoPlayerComponent
+export default VideoPlayerComponent;
--- src/container/VideoPlayerContainer.js
+++ src/container/VideoPlayerContainer.js
@@ -1,52 +1,61 @@
-import React, {useState, useEffect, useRef} from "react";
-import VideoPlayerComponent from "../component/VideoPlayerComponent"
+import React, { useEffect, useRef } from 'react';
+import VideoPlayerComponent from '../component/VideoPlayerComponent';
 
 const VideoPlayerContainer = (props) => {
-    
-    const videoType = "trial" // need to change to be props
-    const videoId = 1 // need to change to be props
-    const videoRef = useRef(null);
+  const videoType = 'trial'; // Replace with your video type
+  const videoId = 1; // Replace with your video ID
+  const videoRef = useRef(null);
 
-    useEffect(()=> {
-        const handleKeyPress = (event) =>{
-            if (event.code === 'ArrowUp') {
-                event.preventDefault();
-                increasePlaybackSpeed();
-            } else if (event.code === 'ArrowDown') {
-                event.preventDefault();
-                decreasePlaybackSpeed();
-            }
-        }
+  const mp3Files = [
+    {
+      src: 'assets/inlineTrialAD/4.513569.mp3',
+      startTime: 4.513569, // Start time in seconds
+      endTime: 20, // End time in seconds
+    },
+    // Add more MP3 files with their timings
+  ];
 
-        document.addEventListener('keydown',handleKeyPress);
-        return () => {
-            document.removeEventListener('keydown',handleKeyPress)
-        }
-    }, []);
-
-    const increasePlaybackSpeed = () => {
-        const videoElement = videoRef.current.video;
-        if (videoElement) {
-          videoElement.playbackRate += 0.25; // Increase the playback rate by 0.1
-        }
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.code === 'ArrowUp') {
+        event.preventDefault();
+        increasePlaybackSpeed();
+      } else if (event.code === 'ArrowDown') {
+        event.preventDefault();
+        decreasePlaybackSpeed();
+      }
     };
 
-    const decreasePlaybackSpeed = () => {
-        const videoElement = videoRef.current.video;
-        if (videoElement) {
-          videoElement.playbackRate -= 0.25; // Decrease the playback rate by 0.1
-        }
-      };
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
 
-    return(
-        <div>
-            <VideoPlayerComponent  
-                videoType = {videoType}
-                videoId = {videoId}
-                videoRef = {videoRef}
-            />
-        </div>
-    )
-}
+  const increasePlaybackSpeed = () => {
+    const videoElement = videoRef.current.video.video;
+    if (videoElement) {
+      videoElement.playbackRate += 0.25; // Increase the playback rate by 0.25
+    }
+  };
 
-export default VideoPlayerContainer
+  const decreasePlaybackSpeed = () => {
+    const videoElement = videoRef.current.video.video;
+    if (videoElement) {
+      videoElement.playbackRate -= 0.25; // Decrease the playback rate by 0.25
+    }
+  };
+
+  return (
+    <div>
+      <VideoPlayerComponent
+        videoType={videoType}
+        videoId={videoId}
+        videoRef={videoRef}
+        mp3Files={mp3Files}
+      />
+    </div>
+  );
+};
+
+export default VideoPlayerContainer;
