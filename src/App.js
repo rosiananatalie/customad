f157626fc71dfe510ce5778a63edcf8a2b778f23
Chong Lee Zhen <kenclz93@gmail.com>
2023-07-03 21:16:56 +0800

    add login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-// import logo from './logo.svg';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { SERVER_URL } from './constants';
 import DashboardComponent from './component/DashboardComponent';
-import KeyboardTest from './component/KeyboardTest';
+import LoginComponent from './component/LoginComponent';
 
 function App() {
-  return (
-    <DashboardComponent />
-  );
+  const [isAuthenticated, setAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => setAuthenticated(boolean);
+
+  async function verifyAuth() {
+    const token = sessionStorage.getItem('token');
+    if (token) {
+      try {
+        const response = await fetch(SERVER_URL + '/auth/verify', {
+            method : 'POST',
+            headers: {
+              'Authorization': 'Bearer ' + token,
+            }
+        });
+        const parseRes = await response.json();
+        setAuthenticated(parseRes.verified);
+      } catch (error) {
+        console.error('Verify auth failed:', error.message);        
+      }
+    } else {
+      setAuthenticated(false);
+    }
+  }
+
+  useEffect(() => {
+    verifyAuth();
+  });
+
+  if (isAuthenticated) {
+    return <DashboardComponent />;
+  } else {
+    return <LoginComponent setAuth={setAuth} />;
+  }
 }
 
 export default App;
---
+++ src/component/LoginComponent.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import { SERVER_URL } from '../constants';
+
+function LoginComponent ({ setAuth }) {
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const formData = {
+                username: document.getElementById('username').value,
+                password: document.getElementById('password').value,
+            };
+
+            const response = await fetch(SERVER_URL + '/auth/login', {
+                method: 'POST',
+                headers: { 'Content-type' : 'application/json' },
+                body: JSON.stringify(formData),
+            });
+
+            const parseRes = await response.json();
+            if (parseRes.token) {
+                setAuth(true);
+                sessionStorage.setItem('token', parseRes.token);
+            } else {
+                setAuth(false);
+                const errorMessage = parseRes.error ? parseRes.error.message : 'Failed to login';
+                document.querySelector('.form-input-hint').innerText = errorMessage;
+            }
+        } catch (error) {
+            console.error('Login failed:', error.message);
+        }
+    }
+
+    return (
+        <div className="container">
+            <div className="col-12 text-center">
+                <h1>Custom AD</h1>
+            </div>
+            <div className="col-4 col-mx-auto">
+                <form className="form-horizontal" onSubmit={handleFormSubmit}>
+                    <div className="form-group">
+                        <div className="col-3 col-sm-12">
+                            <label className="form-label" htmlFor="username">Username</label>
+                        </div>
+                        <div className="col-9 col-sm-12">
+                            <input
+                                className="form-input"
+                                type="text"
+                                id="username"
+                                placeholder="Username"
+                            />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-3 col-sm-12">
+                            <label className="form-label" htmlFor="password">Password:</label>
+                        </div>
+                        <div className="col-9 col-sm-12">
+                            <input
+                                className="form-input"
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                            >
+                            </input>
+                        </div>
+                    </div>
+                    <div className="col-12 text-center">
+                        <button type="submit" className="btn btn-primary">
+                            Log in
+                        </button>
+                    </div>
+                    <div className="col-12 has-error">
+                        <div className="form-input-hint"></div>
+                    </div>
+                </form>
+                {/* TODO: create new account
+                <div class="divider text-center" data-content="OR"></div>
+                <div className="col-12 text-center">
+                    <button type="button" className="btn btn-primary">
+                        Create new account
+                    </button>
+                </div>
+                */}
+            </div>
+        </div>
+    )
+}
+
+export default LoginComponent
