777091485dfaa71afeed9244bd14de019dc793bf
rosiananatalie <rosiananatalie15@gmail.com>
2023-07-24 19:57:08 -0400

    fix the trial video sounds

--- customad_backend/db.js
+++ customad_backend/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
  
 const pool = new Pool({
-  host: 'localhost',
+  host: 'customad.clbpwmmz6ntx.ap-southeast-1.rds.amazonaws.com',
   port: 5432,
   user: 'postgres',
-  password: 'root',
+  password: 'postgresroot',
   database: 'customad',
 });
 
