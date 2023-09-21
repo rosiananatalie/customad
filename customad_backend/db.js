fa012db433064d67fa112eb599fd238f29114a7b
Chong Lee Zhen <kenclz93@gmail.com>
2023-09-21 14:46:03 +0800

    revert

--- customad_backend/db.js
+++ customad_backend/db.js
@@ -2,9 +2,11 @@ const Pool = require("pg").Pool;
  
 const pool = new Pool({
   host: 'localhost',
+  // host: 'customad.clbpwmmz6ntx.ap-southeast-1.rds.amazonaws.com',
   port: 5432,
   user: 'postgres',
   password: 'root',
+  // password: 'postgresroot',
   database: 'customad',
 });
 
--- src/component/DashboardComponent.js
+++ src/component/DashboardComponent.js
@@ -252,11 +252,7 @@ function DashboardComponent({ displayName, videos, handleLogOut }) {
                         const input = document.getElementById(selected);
                         input.click();
                         input.parentElement.style.border = BORDER_STYLE;
-<<<<<<< HEAD
-                        const utterSelection = selected === voice.Synthesizer ? "synthesizer" : selected ; 
-=======
-                        const utterSelection = voice === Voice.Synthesizer ? 'synthesizer' : voice;
->>>>>>> 32ff1185368ee6687a04bc30ca5542633db54484
+                        const utterSelection = selected === Voice.Synthesizer ? 'synthesizer' : selected;
                         utterThis(`${utterSelection} is selected.`);
                     } else if (customization === PresentationCustomization.Gender) {
                         removeSelections();
@@ -317,11 +313,7 @@ function DashboardComponent({ displayName, videos, handleLogOut }) {
                         const input = document.getElementById(selected);
                         input.click();
                         input.parentElement.style.border = BORDER_STYLE;
-<<<<<<< HEAD
-                        const utterSelection = selected === voice.Synthesizer ? "synthesizer" : selected ;
-=======
-                        const utterSelection = voice === Voice.Synthesizer ? 'synthesizer' : voice;
->>>>>>> 32ff1185368ee6687a04bc30ca5542633db54484
+                        const utterSelection = selected === Voice.Synthesizer ? 'synthesizer' : selected;
                         utterThis(`${utterSelection} is selected.`);
                     } else if (customization === PresentationCustomization.Gender) {
                         removeSelections();
